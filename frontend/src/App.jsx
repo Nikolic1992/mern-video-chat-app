b54fb5163d391f5357f07fbd584d7a662a92aeac
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-07-13 13:46:57 +1000

    feat: add routing structure and toast notifications test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import toast, { Toaster } from "react-hot-toast";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="h-screen" data-theme="night">
+      <button onClick={() => toast.success("Hello World!")}>CLICK ME</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
